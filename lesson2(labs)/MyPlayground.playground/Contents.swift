d17b21a51583b2c37688b9aaad709071d7285f84
arman <arman@Armans-MacBook-Pro.local>
2024-01-10 16:39:11 +0600

    лабораторка1

---
+++ lesson2(labs)/MyPlayground.playground/Contents.swift
@@ -0,0 +1,70 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print(greeting)
+
+// Задание 1
+
+print("hello, World!")
+print("Love iOS")
+print("let' go!")
+
+
+// Задание 2
+
+var friends = 50
+friends = 10
+var nonActiveFriends = 15
+
+let updatedFriendsList = friends - nonActiveFriends
+
+print(updatedFriendsList)
+
+//Значение во friend нельзя поменять поскольку это константа, поэтому меняем на переменную
+
+//var friends = 50
+//friends = 10
+
+
+// Задание 3
+
+let goalSteps = 10000
+
+print(goalSteps)
+
+// Задание 4
+
+var schooling = 11
+schooling = schooling + 1
+
+print(schooling)
+
+//Потому что у переменной могут быть доп. значения, а у константы нет
+
+
+// Задание 5
+
+var steps = 0
+
+if steps >= 2000 {
+    print("Хорошая работа! Вы уже на пути к своей цели")
+} else {
+    print("Еще походи плиз!")
+}
+
+
+// Задание 6
+
+var name = "String"
+
+name = "Жаик"
+
+print(name)
+
+
+// Задание 7
+
+var distanceTraveled: Double = 0
+distanceTraveled = 54.3
+
+print(distanceTraveled)
